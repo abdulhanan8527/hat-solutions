d18ed6dd450c3b4d713ac4e6ef039d9d8227e6ba
abdulhanan8527 <honeykhan8527@gmail.com>
2025-04-15 19:20:57 +0500

    code okay

--- hat_solutions/settings.py
+++ hat_solutions/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,7 +29,6 @@ SECRET_KEY = 'django-insecure-!x%m7qhtds&9e^kxl7u-foy15^c^0ipfyck@gpv%$1e6=!%q_j
 DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
-
 
 # Application definition
 
