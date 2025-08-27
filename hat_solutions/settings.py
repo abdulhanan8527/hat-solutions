cb3fa52d07ecd4aa4505ea7bfeba853211b97391
abdulhanan8527 <honeykhan8527@gmail.com>
2025-08-27 20:24:39 +0500

    ChatGPT

--- hat_solutions/settings.py
+++ hat_solutions/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# STATIC_URL = 'static/'
+STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'hat_app/static'),
