fd3a873208e38821d75c3c662a87bac2294746ff
abdulhanan8527 <honeykhan8527@gmail.com>
2025-08-27 21:03:21 +0500

    DeepSeek

--- hat_solutions/settings.py
+++ hat_solutions/settings.py
@@ -162,7 +162,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # For production
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'hat_app/static'),
 ]
